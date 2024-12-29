fb15639c7c05440e792b03a2622fedd7a6c63896
joao9856 <joao-mm-pereira@hotmail.com>
2024-12-29 16:47:27 +0000

    own solution to L 11 on bookbot

--- main.py
+++ main.py
@@ -2,15 +2,35 @@ def main():
     text = text_from_file("books/frankenstein.txt")
     text_len = word_count(text)
     char_count = char_appearence(text)
-    print(char_count)
+    conv_dict = conv(char_count)
+    conv_dict.sort(reverse=True, key=sort_on)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{text_len} words found in the document")
+    print()
+    for char in conv_dict: 
+        print(f"The '{char["name"]}' character was found {char["num"]} times")
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def conv(to_conv):
+    converted = []
+    for char in to_conv:
+        if char.isalpha():
+            converted.append({"name": char, "num": to_conv[char]})
+    return converted
 
 
 def text_from_file(path):
     with open(path) as f:
         return f.read()
+    
 
 def word_count(to_count):
     return len(to_count.split())
+
 
 def char_appearence(to_count_char):
     char_count = {}
@@ -21,4 +41,6 @@ def char_appearence(to_count_char):
         else:
             char_count[char] = 1
     return char_count
+
+
 main()
